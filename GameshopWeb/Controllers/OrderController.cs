3133c180b2c99dd6dfb2b92050ce6581d11836f5
tvbegovic <prof@KAT21>
2023-01-13 20:21:45 +0100

    create order

---
+++ GameshopWeb/Controllers/OrderController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace GameshopWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private IConfiguration _configuration;
+        public OrderController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost("")]
+        public Order Create(Order order)
+        {
+            using (var conn = new SqlConnection(_configuration.GetConnectionString("gameshop")))
+            {
+                conn.Open();
+                var tr = conn.BeginTransaction();
+                try
+                {
+                    var userSql = @"INSERT INTO [dbo].[User]
+                        ([firstname],[lastname],[address],[email],[password],[admin],[City]) OUTPUT inserted.id
+                        VALUES
+                        (@firstname,@lastname, @address, @email, @password, @admin, @city)";
+                    order.IdUser = conn.ExecuteScalar<int>(userSql, order.User,tr);
+                    order.User.Id = order.IdUser.Value;
+                    var orderSql = @"INSERT INTO [Order] (idUser, dateOrdered) OUTPUT inserted.id
+                        VALUES(@idUser, @dateOrdered)";
+                    order.DateOrdered = DateTime.Now;
+                    order.Id = conn.ExecuteScalar<int>(orderSql, order, tr);
+                    var detailSql = @"INSERT INTO OrderDetail(idOrder, idGame, quantity, unitprice) OUTPUT inserted.id
+                        VALUES(@idOrder, @idGame, @quantity, @unitprice)";
+                    foreach (var detail in order.Details)
+                    {
+                        detail.IdOrder = order.Id;
+                        detail.Id = conn.ExecuteScalar<int>(detailSql, detail, tr);
+                    }
+                    tr.Commit();
+                    return order;
+                }
+                catch (Exception)
+                {
+                    tr.Rollback();
+                    throw;
+                }
+            }
+        }
+    }
+}
--- GameshopWeb/Klase.cs
+++ GameshopWeb/Klase.cs
@@ -17,6 +17,8 @@
         public int? IdEmployee { get; set; }
         public DateTime? DateOrdered { get; set; }
         public DateTime? DateSent { get; set; }
+        public User User { get; set; }
+        public List<OrderDetail> Details { get; set; }
     }
     public class OrderDetail
     {
@@ -33,7 +35,7 @@
         public string Lastname { get; set; }
         public string Address { get; set; }
         public string Email { get; set; }
-        public string Password { get; set; }
+        public string? Password { get; set; }
         public bool? Admin { get; set; }
         public string City { get; set; }
     }
